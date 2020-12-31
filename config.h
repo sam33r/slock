bfb22d81b482576e554148cc46f9cd92e22536ee
Sameer <sameer.ahuja@gmail.com>
2020-12-30 16:22:30 -0800

    Change blocks configuration to be less trippy.

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static short int blocks_enabled = 1;
 // The screen is divided into a grid of <blocks_count>x<blocks_count>
 // Each rectangle's size is random number of blocks in this grid,
 // limited to <max_size_in_blocks>.
-static const int blocks_count = 20;
-static const int max_size_in_blocks = 5;
+static const int blocks_count = 10;
+static const int max_size_in_blocks = 3;
 // Clearing blocks is more secure, but won't create a mosaic
 // masterpiece.
 static short int clear_blocks = 0;
